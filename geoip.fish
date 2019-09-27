4b5d8cb9bee5f9586ff384ea1eef602963e4cc8e
Gergely Tankovics <gergely.tankovics@gmail.com>
2019-09-27 15:48:12 +0200

    add check option

--- geoip.fish
+++ geoip.fish
@@ -1,7 +1,7 @@
 function geoip --description "IP details from ipstack.com"
 
 	if test $argv
-		if string match --regex --quiet '\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b' $argv 
+		if string match --regex --quiet '\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b' $argv || string match --quiet "check" $argv
 			if test -n "$ipstack_api_key"
 				curl -s "http://api.ipstack.com/$argv?access_key=$ipstack_api_key" | jq 
 			else
