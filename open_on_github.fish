dcb787ed6c0d0e4806708bda00f9dd3526eabd01
Gergely Tankovics <gergely.tankovics@gmail.com>
2019-10-04 12:02:37 +0200

    add branch into remote_url

--- open_on_github.fish
+++ open_on_github.fish
@@ -1,9 +1,10 @@
 function open_on_github --description "Open repo on GitHub if remote available"
 
-	set -l remote (git remote -v | grep fetch | awk '{print $2}')
+	set -l remote (git remote -v | grep fetch | awk '{print $2}' | sed 's/git@/https:\/\//' | sed 's/com:/com\//')
+	set -l branch (git branch -a | awk '/\*/{print$2}')
 
 	if string length -q $remote 
-		set -l remote_url (echo -n $remote | sed 's/git@/https:\/\//' | sed 's/com:/com\//' )
+		set -l remote_url (string join "/" "$remote" "tree" "$branch")
 		echo $remote_url
 		open $remote_url
 	else
