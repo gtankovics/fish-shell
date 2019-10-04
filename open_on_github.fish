bcdb67ebb2b771f4c6605df2e010ec9c5ccdbb24
Gergely Tankovics <gergely.tankovics@gmail.com>
2019-10-04 12:10:20 +0200

    handling not repo folders

--- open_on_github.fish
+++ open_on_github.fish
@@ -1,14 +1,20 @@
 function open_on_github --description "Open repo on GitHub if remote available"
 
-	set -l remote (git remote -v | grep fetch | awk '{print $2}' | sed 's/git@/https:\/\//' | sed 's/com:/com\//')
-	set -l branch (git branch -a | awk '/\*/{print$2}')
+	if test -e ./.git
 
-	if string length -q $remote 
-		set -l remote_url (string join "/" "$remote" "tree" "$branch")
-		echo $remote_url
-		open $remote_url
+		set -l remote (git remote -v | grep fetch | awk '{print $2}' | sed 's/git@/https:\/\//' | sed 's/com:/com\//')
+
+		if string length -q $remote 
+			set -l branch (git branch -a | awk '/\*/{print$2}')
+			set -l remote_url (string join "/" "$remote" "tree" "$branch")
+			echo $remote_url
+			open $remote_url
+		else
+			echo "There is no remote."
+		end
 	else
-		echo "There is no remote."
+
+		echo "This is not a git repo."
 	end
 
 end
