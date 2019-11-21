2023a438dc575e315dbcaea6fa0848f4b86bd7b5
Gergely Tankovics <gergely.tankovics@gmail.com>
2019-11-21 17:52:13 +0100

    remove SSH check

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -3,7 +3,7 @@ function fish_prompt --description 'Write out the prompt'
 
     # detailed prompt
 
-    if test -n "$fish_prompt_detailed" && test "$TERM_PROGRAM" != "vscode" && test -n "SSH_TTY"
+    if test -n "$fish_prompt_detailed" && test "$TERM_PROGRAM" != "vscode"
 
         if not test -n "$fish_prompt_detailed_last_check"
             set -U fish_prompt_detailed_last_check (date +%s)
