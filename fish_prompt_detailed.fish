48a38562cb800c51920df9374fc1d5caa8ccf127
Gergely Tankovics <gergely.tankovics@gmail.com>
2021-03-21 00:14:17 +0100

    clear gcloud and kubectl (require chenv)

--- fish_prompt_detailed.fish
+++ fish_prompt_detailed.fish
@@ -1,9 +1,12 @@
 function fish_prompt_detailed -d "Turns on/off fish_prompt_detailed"
     if test "$fish_prompt_detailed"
         set -e fish_prompt_detailed
-        echo 'Detailed prompt turned OFF.'
+        set -xU LAST_GOOGLE_CONFIG $GOOGLE_CONFIG
+        fchenv clear
+        echo 'Detailed prompt turned OFF. `gcloud` and `kubectl` unset.'
     else
         set -xU fish_prompt_detailed true
+        fchenv set $LAST_GOOGLE_CONFIG
         echo 'Detailed prompt turned ON.'
     end
 end
