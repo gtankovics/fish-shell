047353254bafb920519f75728a54118901adf460
Gergely Tankovics <gergely.tankovics@gmail.com>
2021-03-20 23:34:28 +0100

    add ACTIVE_DOMAIN_SUFFIX

--- iterm2_update_user_vars.fish
+++ iterm2_update_user_vars.fish
@@ -9,9 +9,9 @@ function iterm2_update_user_vars --description "This function updates user varia
 	iterm2_set_user_var GOOGLE_ZONE $GOOGLE_ZONE
 	# (gcloud config configurations list --filter 'is_active=true' --format 'value(properties.compute.zone)')
 	
+	iterm2_set_user_var ACTIVE_DOMAIN_SUFFIX $ACTIVE_DOMAIN_SUFFIX
 	iterm2_set_user_var K8S_CLUSTER_SHORT $K8S_CLUSTER_SHORT
 	iterm2_set_user_var K8S_CLUSTER_VERSION $K8S_CLUSTER_VERSION
-
 
 	iterm2_set_user_var NVM_CURRENT_VERSION $NVM_CURRENT_VERSION
 
