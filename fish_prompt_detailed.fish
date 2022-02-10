01e1d2d6e72e7362b9331492c6aad72a1b232e35
Gergely Tankovics <gergely.tankovics@gmail.com>
2022-02-10 12:56:18 +0100

    add starship toggle

--- fish_prompt_detailed.fish
+++ fish_prompt_detailed.fish
@@ -3,9 +3,15 @@ function fish_prompt_detailed -d "Turns on/off fish_prompt_detailed"
         set -e fish_prompt_detailed
         set -xU LAST_GOOGLE_CONFIG $GOOGLE_CONFIG
         fchenv clear
+        if which starship
+            starship toogle gcloud
+        end
         echo 'Detailed prompt turned OFF. `gcloud` and `kubectl` unset.'
     else
         set -xU fish_prompt_detailed true
+        if which starship
+            starship toogle gcloud
+        end
         fchenv set $LAST_GOOGLE_CONFIG
         echo 'Detailed prompt turned ON.'
     end
