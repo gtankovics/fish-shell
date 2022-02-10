4225834cf765cb1ec47bf7808f527c8c85dd40eb
Gergely Tankovics <gergely.tankovics@gmail.com>
2022-02-10 12:59:38 +0100

    typo fix

--- fish_prompt_detailed.fish
+++ fish_prompt_detailed.fish
@@ -4,13 +4,13 @@ function fish_prompt_detailed -d "Turns on/off fish_prompt_detailed"
         set -xU LAST_GOOGLE_CONFIG $GOOGLE_CONFIG
         fchenv clear
         if which starship
-            starship toogle gcloud
+            starship toggle gcloud
         end
         echo 'Detailed prompt turned OFF. `gcloud` and `kubectl` unset.'
     else
         set -xU fish_prompt_detailed true
         if which starship
-            starship toogle gcloud
+            starship toggle gcloud
         end
         fchenv set $LAST_GOOGLE_CONFIG
         echo 'Detailed prompt turned ON.'
