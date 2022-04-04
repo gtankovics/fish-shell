cba436369f7bc36d4a9a3f359fb41ea84e50b8b9
Gergely Tankovics <gergely.tankovics@gmail.com>
2022-04-04 20:17:49 +0200

    fix Ctrl+C issue, colorize

--- terraform.fish
+++ terraform.fish
@@ -1,21 +1,24 @@
 function terraform --description "Check local terraform.sh or run the general one"
+	set_color -b brred -i brwhite
 	if test -f "./terraform.sh"
 		# echo "Local 'terraform.sh' found"
 		if test -x "./terraform.sh"
 			# echo "'terraform.sh' is executeable. Run it !"
-			echo "Running terraform from local 'terraform.sh'"
-			exec ./terraform.sh $argv
+			echo " Running terraform from local 'terraform.sh' "
+			set_color normal
+			./terraform.sh $argv
 		else
-			echo "Running terraform from local 'terraform.sh' is not possible because it's not executeable."
+			echo " Running terraform from local 'terraform.sh' is not possible because it's not executeable. "
 		end
 	else
 		if find . -name "*.tf" -d 1 -type f > /dev/null
 			set -l terraform_path (which terraform)
-			echo "there's no 'terraform.sh' found. But found '*.tf' files. Run terraform from $terraform_path"
+			echo " There's no 'terraform.sh' found. But found '*.tf' files. Run terraform from $terraform_path "
+			set_color normal
 			$terraform_path $argv
 		else
-			echo "This folder is not related to 'terraform'."
+			echo " This folder is not related to 'terraform'. "
 		end
 	end
-
+	set_color normal
 end
