b463f5bdccb9a4847d2656a3a4c60f514b845bd1
Gergely Tankovics <gtankovics@graphisoft.com>
2022-10-17 14:30:26 +0200

    cleanbranches small fixes

--- cleanbranches.fish
+++ cleanbranches.fish
@@ -1,5 +1,9 @@
 function cleanbranches -d "Removes local branches which does not have remote"
+    git fetch --all --tags --prune
     for branch in (git branch -vv | awk '/gone/{print$1}')
-        git branch -D $branch
+        if test "$branch" != "*"
+            git branch -D $branch
+        end
     end
+    echo Done.
 end
