6fecb3cc8a2f5a47e855b1edbedd63a8b8740620
Gergely Tankovics <gtankovics_local@HU01LM25000364.local>
2025-09-15 21:20:08 +0200

    fix IP and PORT for HOMEY

--- charge_battery.sh
+++ charge_battery.sh
@@ -1,19 +1,22 @@
-#!/usr/bin/env fish 
+#!/bin/bash
 
-# Get Homey Token from Key chain
-# https://scriptingosx.com/2021/04/get-password-from-keychain-in-shell-scripts/
-set -l homeyToken (security find-generic-password -w -s 'Homey Token' -a 'homey')
+# set -ex
 
-# check battery percentage
-if test (pmset -g batt | grep -Eo "\d+%" | cut -d % -f1) -lt 20
-    # get plug state
-    set -l macPlugState (curl --location --silent 'http://192.168.0.150/api/manager/devices/device/6dea7dd6-f94b-4d2d-a021-a6433c1e6461/' \
-        --header 'Content-Type: application/json' \
-        --header 'Authorization: Bearer '$homeyToken'' | jq '.capabilitiesObj.onoff.value')
-    if not $macPlugState 
-        curl --location --silent --request PUT 'http://192.168.0.150/api/manager/devices/device/6dea7dd6-f94b-4d2d-a021-a6433c1e6461/capability/onoff/' \
-            --header 'Content-Type: application/json' \
-            --header 'Authorization: Bearer '$homeyToken'' \
-            --data '{"value": true}' >> /dev/null
-    end
-end
+HOMEY_IP="192.168.0.150"
+HOMEY_PORT="8441"
+
+# get battery percentage
+BATTERY_PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d % -f1)
+
+# get plug state
+MAC_PLUG_STATE=$(curl --location --silent "http://$HOMEY_IP:$HOMEY_PORT/lab/macplug" | /opt/homebrew/bin/jq '.data.state')
+
+if [ $BATTERY_PERCENTAGE -lt 20 ]; then
+    if [ "$MAC_PLUG_STATE" == "false" ]; then
+        curl --location -X POST "http://$HOMEY_IP:$HOMEY_PORT/lab/macplug"
+    fi
+elif [ $BATTERY_PERCENTAGE -gt 80 ]; then
+    if [ "$MAC_PLUG_STATE" = "true" ]; then
+        curl --location -X POST "http://$HOMEY_IP:$HOMEY_PORT/lab/macplug"
+    fi
+fi
